c3c59411f60c1b00d2489c0e608a02823ec90e19
MargotGuo <gyqgdn@126.com>
2020-02-18 23:04:01 +0800

    [Guo Yuanqi]{feat}: practice for collection

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,16 +1,23 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Practice1 {
-    public static void main(String[] args) {
-        System.out.println(randomNumber());
-    }
+  public static void main(String[] args) {
+    System.out.println(randomNumber());
+  }
 
-    /**
-     *  生成10个0到20的随机整数，要求不能重复
-     */
-    public static Collection<Integer> randomNumber() {
-        return null;
+  /**
+   * 生成10个0到20的随机整数，要求不能重复
+   */
+  public static Collection<Integer> randomNumber() {
+    Collection<Integer> randomNumbers = new HashSet<>();
+    Random random = new Random();
+    while (randomNumbers.size() < 10) {
+      randomNumbers.add(random.nextInt(21));
     }
+    return randomNumbers;
+  }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,19 +1,46 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
-    public static void main(String[] args) {
-        //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
-    }
+  public static void main(String[] args) {
+    //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
 
-    /**
-     * 创建一个List 然后在List中添加0到100整数
-     */
-    public static List<Integer> getNumbers() {
-        return null;
+    List<Integer> list = getNumbers();
+
+    // for 循环
+    for (int i = 0; i < list.size(); i++) {
+      System.out.printf("%d ", list.get(i));
     }
+    System.out.println();
+
+    // 增强 for 循环
+    for (int number : list) {
+      System.out.printf("%d ", number);
+    }
+    System.out.println();
+
+    //迭代器
+    Iterator<Integer> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      System.out.printf("%d ", iterator.next());
+    }
+    System.out.println();
+  }
+
+  /**
+   * 创建一个List 然后在List中添加0到100整数
+   */
+  public static List<Integer> getNumbers() {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i <= 100; i++) {
+      list.add(i);
+    }
+    return list;
+  }
 
 
 }
